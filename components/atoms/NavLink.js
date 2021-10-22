16e7ebed50ba1fec947ed9fcd3b83432e838e584
Jan Vítů <janvitu@eduroam158-115.nbk.vse.cz>
2021-10-22 13:30:01 +0200

    a tag changed to Link object and className fix in NavBar.js

--- components/atoms/NavLink.js
+++ components/atoms/NavLink.js
@@ -1,9 +1,10 @@
+import Link from "next/link";
 export function NavLink({ href, children }) {
 	return (
-		<a href={href} className="group relative hover:text-main-600 py-3">
+		<Link href={href} className="group relative hover:text-main-600 py-3" passHref>
 			{children}
 			<div className="w-0 md:group-hover:w-full h-1 bg-main-600 transition-all" />
-		</a>
+		</Link>
 	);
 }
 
--- components/organisms/NavBar.js
+++ components/organisms/NavBar.js
@@ -1,6 +1,7 @@
 import { ButtonLinkFilledBase, ButtonLinkOutlineBase } from "@components/atoms";
 import { NavStatics } from "@components/molecules";
 import { Logo } from "@components/partials/Logo";
+import Link from "next/link";
 
 const ROUTES = [
 	{ name: "partner", path: "/stante-se-partnerem", text: "Staňte se partnerem" },
@@ -9,17 +10,17 @@ const ROUTES = [
 ];
 export function Navbar() {
 	return (
-		<div class="relative bg-white">
-			<div class="max-w-7xl mx-auto px-4 sm:px-6">
-				<div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-					<div class="flex justify-start lg:w-0 lg:flex-1">
-						<a href="/">
-							<span class="sr-only">Fit.Me</span>
+		<div className="relative bg-white">
+			<div className="max-w-7xl mx-auto px-4 sm:px-6">
+				<div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+					<div className="flex justify-start lg:w-0 lg:flex-1">
+						<Link href="/" passHref>
+							<span className="sr-only">Fit.Me</span>
 							<Logo />
-						</a>
+						</Link>
 					</div>
 					<NavStatics routes={ROUTES} />
-					<div class="hidden md:flex space-x-2 items-center justify-end md:flex-1 lg:w-0">
+					<div className="hidden md:flex space-x-2 items-center justify-end md:flex-1 lg:w-0">
 						<ButtonLinkOutlineBase>Registrace</ButtonLinkOutlineBase>
 						<ButtonLinkFilledBase>Přihlásit se</ButtonLinkFilledBase>
 					</div>
