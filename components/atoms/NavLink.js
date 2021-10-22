b1ac816509077bf7b5b8ff013e333e8a43f0b2d3
albertmirek <albert.mirek@seznam.cz>
2021-10-22 17:45:33 +0200

    NavLink Fix

--- components/atoms/NavLink.js
+++ components/atoms/NavLink.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
 export function NavLink({ href, children }) {
 	return (
-		<Link href={href} className="group relative hover:text-main-600 py-3" passHref>
-			{children}
-			<div className="w-0 md:group-hover:w-full h-1 bg-main-600 transition-all" />
+		<Link href={href} passHref>
+			<div className="group relative hover:text-main-600 py-3">
+				{children}
+				<div className=" w-0 md:group-hover:w-full h-1 bg-main-600 transition-all" />
+			</div>
 		</Link>
 	);
 }
