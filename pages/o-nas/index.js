0c5df7c99ca4ec086afc38c9435b87e81d9d7788
albertmirek <albert.mirek@seznam.cz>
2021-10-23 11:19:52 +0200

    Addedd section " Náš přístup" in about page

--- pages/o-nas/index.js
+++ pages/o-nas/index.js
@@ -1,5 +1,40 @@
 import { StaticSite } from "@components/templates";
 
 export default function About() {
-	return <StaticSite></StaticSite>;
+	return (
+		<StaticSite>
+			{/* Nadpis */}
+
+			{/* "V čem jsme jiní" section */}
+
+			{/* CTA: "Posuňte svůj workout na novou úroveň" */}
+
+			{/* "Náš přístup" section */}
+
+			<div className="bg-white">
+				<div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+					<div className="text-center">
+						<h2 className="text-base font-semibold text-main-600 tracking-wide uppercase">
+							Spolupráce
+						</h2>
+						<p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+							Náš přístup
+						</p>
+						<p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+							Start building for free, then add a site plan to go live. Account plans unlock
+							additional features.
+						</p>
+					</div>
+				</div>
+			</div>
+
+			{/* "Náš tým" section */}
+
+			{/* "Píší o nás" section */}
+
+			{/* "Kontaktujte nás" section */}
+
+			{/* FAQ */}
+		</StaticSite>
+	);
 }
