e334d0ec7cd93bad52f674d2b0d6c18e7e6eeb17
Jan Vítů <vitu.jan1@gmail.com>
2021-10-23 16:13:28 +0200

    Fix cursor display on hover in NavLink

--- components/atoms/NavLink.js
+++ components/atoms/NavLink.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 export function NavLink({ href, children }) {
 	return (
 		<Link href={href} passHref>
-			<div className="group relative hover:text-main-600 py-3">
+			<div className="group relative cursor-pointer hover:text-main-600 py-3">
 				{children}
 				<div className=" w-0 md:group-hover:w-full h-1 bg-main-600 transition-all" />
 			</div>
