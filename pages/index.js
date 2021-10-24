9613debce08f3fdbc8612f0715cd961b47cd0baf
Jan Vítů <vitu.jan1@gmail.com>
2021-10-24 12:27:05 +0200

    Static pages basic content added

--- pages/index.js
+++ pages/index.js
@@ -8,16 +8,35 @@ import {
 import { StaticSite } from "@components/templates";
 
 export default function Home() {
-	// 	<script>
-	// function myMap() {
-	// var mapProp= {
-	//   center:new google.maps.LatLng(51.508742,-0.120850),
-	//   zoom:5,
-	// };
-	// var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-	// }
-	// </script>
+	/**
+	 * Coppy to clipboard function ussed for contact information coppy
+	 * @param  {} event
+	 */
+	function copyToClip(event) {
+		let r = document.createRange();
 
+		const target = event.currentTarget;
+		const coppyElem = target.querySelector("span");
+
+		r.selectNode(coppyElem);
+		window.getSelection().addRange(r);
+		document.execCommand("copy");
+
+		window.getSelection().removeAllRanges();
+	}
+
+	function handleCopyClick(e) {
+		copyToClip(e);
+		const copyBanners = e.currentTarget.querySelectorAll("[data-coppy]");
+		copyBanners.forEach((element) => {
+			element.style.opacity = "1";
+		});
+		setTimeout(() => {
+			copyBanners.forEach((element) => {
+				element.style.opacity = "";
+			});
+		}, 1000);
+	}
 	const myMap = () => {
 		var mapProp = {
 			center: new google.maps.LatLng(51.508742, -0.12085),
@@ -232,7 +251,6 @@ export default function Home() {
 			</section>
 
 			{/* STATS Split with Image */}
-
 			<div className="relative bg-white">
 				<div className="h-56 bg-main-600 sm:h-72 lg:absolute lg:left-0 lg:h-full lg:w-1/2">
 					<img
@@ -353,6 +371,336 @@ export default function Home() {
 					/>
 				</div>
 			</div>
+
+			<section className="bg-white">
+				<div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+					<div className="divide-y-2 divide-gray-200">
+						<div className="lg:grid lg:grid-cols-3 lg:gap-8">
+							<div>
+								<h2 className="text-2xl font-extrabold text-gray-900 sm:text-3xl">Get in touch</h2>
+								<div className=" h-2 w-16 mt-2 mb-4 bg-main-800 rounded-full"></div>
+							</div>
+							<div className="mt-8 grid grid-cols-1 gap-12 sm:grid-cols-2 sm:gap-x-8 sm:gap-y-12 lg:mt-0 lg:col-span-2">
+								<div>
+									<div className="flex gap-4 items-center">
+										<div>
+											<img
+												className="rounded-full h-14 w-14 object-cover border-2 border-main-800"
+												src="https://source.unsplash.com/random/?male,face"
+												alt="Person"
+											/>
+										</div>
+										<div>
+											<h3 className="text-lg leading-6 font-bold text-gray-900">John Doe</h3>
+											<h4 className="text-gray-600">Batman, Arkham City</h4>
+										</div>
+									</div>
+									<dl className="mt-2 text-base text-gray-500">
+										<div>
+											<dt className="sr-only">Email</dt>
+											<dd onClick={handleCopyClick} className="group flex hover:cursor-pointer">
+												<span className="">john@doe.com</span>
+												<div className="relative flex items-center">
+													<div
+														data-coppy=""
+														className="absolute inset-x-0 bottom-full mb-1 flex justify-center opacity-0 transition-opacity duration-200"
+													>
+														<span className="sr-only">Copy code</span>
+														<span className="bg-gray-900 text-white rounded-md text-[0.625rem] leading-4 tracking-wide font-semibold uppercase py-1 px-3 filter drop-shadow-md">
+															<svg
+																width="16"
+																height="6"
+																viewBox="0 0 16 6"
+																className="text-gray-900 absolute top-full left-1/2 -mt-px -ml-2"
+															>
+																<path
+																	fillRule="evenodd"
+																	clipRule="evenodd"
+																	d="M15 0H1V1.00366V1.00366V1.00371H1.01672C2.72058 1.0147 4.24225 2.74704 5.42685 4.72928C6.42941 6.40691 9.57154 6.4069 10.5741 4.72926C11.7587 2.74703 13.2803 1.0147 14.9841 1.00371H15V0Z"
+																	fill="currentColor"
+																></path>
+															</svg>
+															Copied
+														</span>
+													</div>
+													<svg
+														data-coppy=""
+														aria-hidden="true"
+														focusable="false"
+														className="w-4 h-auto ml-2 opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition-opacity duration-200"
+														role="img"
+														xmlns="http://www.w3.org/2000/svg"
+														viewBox="0 0 512 512"
+													>
+														<path
+															fill="currentColor"
+															d="M464 0H144c-26.51 0-48 21.49-48 48v48H48c-26.51 0-48 21.49-48 48v320c0 26.51 21.49 48 48 48h320c26.51 0 48-21.49 48-48v-48h48c26.51 0 48-21.49 48-48V48c0-26.51-21.49-48-48-48zM362 464H54a6 6 0 0 1-6-6V150a6 6 0 0 1 6-6h42v224c0 26.51 21.49 48 48 48h224v42a6 6 0 0 1-6 6zm96-96H150a6 6 0 0 1-6-6V54a6 6 0 0 1 6-6h308a6 6 0 0 1 6 6v308a6 6 0 0 1-6 6z"
+														></path>
+													</svg>
+												</div>
+											</dd>
+										</div>
+										<div className="mt-1">
+											<dt className="sr-only">Phone number</dt>
+											<dd onClick={handleCopyClick} className="group flex hover:cursor-pointer">
+												<span>+420 777 777 777</span>
+												<div className="relative flex items-center">
+													<div
+														data-coppy=""
+														className="absolute inset-x-0 bottom-full mb-1 flex justify-center opacity-0 transition-opacity duration-200"
+													>
+														<span className="sr-only">Copy code</span>
+														<span className="bg-gray-900 text-white rounded-md text-[0.625rem] leading-4 tracking-wide font-semibold uppercase py-1 px-3 filter drop-shadow-md">
+															<svg
+																width="16"
+																height="6"
+																viewBox="0 0 16 6"
+																className="text-gray-900 absolute top-full left-1/2 -mt-px -ml-2"
+															>
+																<path
+																	fillRule="evenodd"
+																	clipRule="evenodd"
+																	d="M15 0H1V1.00366V1.00366V1.00371H1.01672C2.72058 1.0147 4.24225 2.74704 5.42685 4.72928C6.42941 6.40691 9.57154 6.4069 10.5741 4.72926C11.7587 2.74703 13.2803 1.0147 14.9841 1.00371H15V0Z"
+																	fill="currentColor"
+																></path>
+															</svg>
+															Copied
+														</span>
+													</div>
+													<svg
+														data-coppy=""
+														aria-hidden="true"
+														focusable="false"
+														className="w-4 h-auto ml-2 opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition-opacity duration-200"
+														role="img"
+														xmlns="http://www.w3.org/2000/svg"
+														viewBox="0 0 512 512"
+													>
+														<path
+															fill="currentColor"
+															d="M464 0H144c-26.51 0-48 21.49-48 48v48H48c-26.51 0-48 21.49-48 48v320c0 26.51 21.49 48 48 48h320c26.51 0 48-21.49 48-48v-48h48c26.51 0 48-21.49 48-48V48c0-26.51-21.49-48-48-48zM362 464H54a6 6 0 0 1-6-6V150a6 6 0 0 1 6-6h42v224c0 26.51 21.49 48 48 48h224v42a6 6 0 0 1-6 6zm96-96H150a6 6 0 0 1-6-6V54a6 6 0 0 1 6-6h308a6 6 0 0 1 6 6v308a6 6 0 0 1-6 6z"
+														></path>
+													</svg>
+												</div>
+											</dd>
+										</div>
+									</dl>
+								</div>
+								<div>
+									<div className="flex gap-4 items-center">
+										<div>
+											<img
+												className="rounded-full h-14 w-14 object-cover border-2 border-main-800"
+												src="https://source.unsplash.com/random/?male,face"
+												alt="Person"
+											/>
+										</div>
+										<div>
+											<h3 className="text-lg leading-6 font-bold text-gray-900">Not Me</h3>
+											<h4 className="text-gray-600">CTO, Apple</h4>
+										</div>
+									</div>
+									<dl className="mt-2 text-base text-gray-500">
+										<div>
+											<dt className="sr-only">Email</dt>
+											<dd onClick={handleCopyClick} className="group flex hover:cursor-pointer">
+												<span className="">not@me.com</span>
+												<div className="relative flex items-center">
+													<div
+														data-coppy=""
+														className="absolute inset-x-0 bottom-full mb-1 flex justify-center opacity-0 transition-opacity duration-200"
+													>
+														<span className="sr-only">Copy code</span>
+														<span className="bg-gray-900 text-white rounded-md text-[0.625rem] leading-4 tracking-wide font-semibold uppercase py-1 px-3 filter drop-shadow-md">
+															<svg
+																width="16"
+																height="6"
+																viewBox="0 0 16 6"
+																className="text-gray-900 absolute top-full left-1/2 -mt-px -ml-2"
+															>
+																<path
+																	fillRule="evenodd"
+																	clipRule="evenodd"
+																	d="M15 0H1V1.00366V1.00366V1.00371H1.01672C2.72058 1.0147 4.24225 2.74704 5.42685 4.72928C6.42941 6.40691 9.57154 6.4069 10.5741 4.72926C11.7587 2.74703 13.2803 1.0147 14.9841 1.00371H15V0Z"
+																	fill="currentColor"
+																></path>
+															</svg>
+															Copied
+														</span>
+													</div>
+													<svg
+														data-coppy=""
+														aria-hidden="true"
+														focusable="false"
+														className="w-4 h-auto ml-2 opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition-opacity duration-200"
+														role="img"
+														xmlns="http://www.w3.org/2000/svg"
+														viewBox="0 0 512 512"
+													>
+														<path
+															fill="currentColor"
+															d="M464 0H144c-26.51 0-48 21.49-48 48v48H48c-26.51 0-48 21.49-48 48v320c0 26.51 21.49 48 48 48h320c26.51 0 48-21.49 48-48v-48h48c26.51 0 48-21.49 48-48V48c0-26.51-21.49-48-48-48zM362 464H54a6 6 0 0 1-6-6V150a6 6 0 0 1 6-6h42v224c0 26.51 21.49 48 48 48h224v42a6 6 0 0 1-6 6zm96-96H150a6 6 0 0 1-6-6V54a6 6 0 0 1 6-6h308a6 6 0 0 1 6 6v308a6 6 0 0 1-6 6z"
+														></path>
+													</svg>
+												</div>
+											</dd>
+										</div>
+										<div className="mt-1">
+											<dt className="sr-only">Phone number</dt>
+											<dd onClick={handleCopyClick} className="group flex hover:cursor-pointer">
+												<span>777 777 777</span>
+												<div className="relative flex items-center">
+													<div
+														data-coppy=""
+														className="absolute inset-x-0 bottom-full mb-1 flex justify-center opacity-0 transition-opacity duration-200"
+													>
+														<span className="sr-only">Copy code</span>
+														<span className="bg-gray-900 text-white rounded-md text-[0.625rem] leading-4 tracking-wide font-semibold uppercase py-1 px-3 filter drop-shadow-md">
+															<svg
+																width="16"
+																height="6"
+																viewBox="0 0 16 6"
+																className="text-gray-900 absolute top-full left-1/2 -mt-px -ml-2"
+															>
+																<path
+																	fillRule="evenodd"
+																	clipRule="evenodd"
+																	d="M15 0H1V1.00366V1.00366V1.00371H1.01672C2.72058 1.0147 4.24225 2.74704 5.42685 4.72928C6.42941 6.40691 9.57154 6.4069 10.5741 4.72926C11.7587 2.74703 13.2803 1.0147 14.9841 1.00371H15V0Z"
+																	fill="currentColor"
+																></path>
+															</svg>
+															Copied
+														</span>
+													</div>
+													<svg
+														data-coppy=""
+														aria-hidden="true"
+														focusable="false"
+														className="w-4 h-auto ml-2 opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition-opacity duration-200"
+														role="img"
+														xmlns="http://www.w3.org/2000/svg"
+														viewBox="0 0 512 512"
+													>
+														<path
+															fill="currentColor"
+															d="M464 0H144c-26.51 0-48 21.49-48 48v48H48c-26.51 0-48 21.49-48 48v320c0 26.51 21.49 48 48 48h320c26.51 0 48-21.49 48-48v-48h48c26.51 0 48-21.49 48-48V48c0-26.51-21.49-48-48-48zM362 464H54a6 6 0 0 1-6-6V150a6 6 0 0 1 6-6h42v224c0 26.51 21.49 48 48 48h224v42a6 6 0 0 1-6 6zm96-96H150a6 6 0 0 1-6-6V54a6 6 0 0 1 6-6h308a6 6 0 0 1 6 6v308a6 6 0 0 1-6 6z"
+														></path>
+													</svg>
+												</div>
+											</dd>
+										</div>
+									</dl>
+								</div>
+								<div>
+									<div className="flex gap-4 items-center">
+										<div>
+											<img
+												className="rounded-full h-14 w-14 object-cover border-2 border-main-800"
+												src="https://source.unsplash.com/random/?male,face"
+												alt="Person"
+											/>
+										</div>
+										<div>
+											<h3 className="text-lg leading-6 font-bold text-gray-900">Not Me</h3>
+											<h4 className="text-gray-600">CTO, Apple</h4>
+										</div>
+									</div>
+									<dl className="mt-2 text-base text-gray-500">
+										<div>
+											<dt className="sr-only">Email</dt>
+											<dd onClick={handleCopyClick} className="group flex hover:cursor-pointer">
+												<span className="">not@me.com</span>
+												<div className="relative flex items-center">
+													<div
+														data-coppy=""
+														className="absolute inset-x-0 bottom-full mb-1 flex justify-center opacity-0 transition-opacity duration-200"
+													>
+														<span className="sr-only">Copy code</span>
+														<span className="bg-gray-900 text-white rounded-md text-[0.625rem] leading-4 tracking-wide font-semibold uppercase py-1 px-3 filter drop-shadow-md">
+															<svg
+																width="16"
+																height="6"
+																viewBox="0 0 16 6"
+																className="text-gray-900 absolute top-full left-1/2 -mt-px -ml-2"
+															>
+																<path
+																	fillRule="evenodd"
+																	clipRule="evenodd"
+																	d="M15 0H1V1.00366V1.00366V1.00371H1.01672C2.72058 1.0147 4.24225 2.74704 5.42685 4.72928C6.42941 6.40691 9.57154 6.4069 10.5741 4.72926C11.7587 2.74703 13.2803 1.0147 14.9841 1.00371H15V0Z"
+																	fill="currentColor"
+																></path>
+															</svg>
+															Copied
+														</span>
+													</div>
+													<svg
+														data-coppy=""
+														aria-hidden="true"
+														focusable="false"
+														className="w-4 h-auto ml-2 opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition-opacity duration-200"
+														role="img"
+														xmlns="http://www.w3.org/2000/svg"
+														viewBox="0 0 512 512"
+													>
+														<path
+															fill="currentColor"
+															d="M464 0H144c-26.51 0-48 21.49-48 48v48H48c-26.51 0-48 21.49-48 48v320c0 26.51 21.49 48 48 48h320c26.51 0 48-21.49 48-48v-48h48c26.51 0 48-21.49 48-48V48c0-26.51-21.49-48-48-48zM362 464H54a6 6 0 0 1-6-6V150a6 6 0 0 1 6-6h42v224c0 26.51 21.49 48 48 48h224v42a6 6 0 0 1-6 6zm96-96H150a6 6 0 0 1-6-6V54a6 6 0 0 1 6-6h308a6 6 0 0 1 6 6v308a6 6 0 0 1-6 6z"
+														></path>
+													</svg>
+												</div>
+											</dd>
+										</div>
+										<div className="mt-1">
+											<dt className="sr-only">Phone number</dt>
+											<dd onClick={handleCopyClick} className="group flex hover:cursor-pointer">
+												<span>777 777 777</span>
+												<div className="relative flex items-center">
+													<div
+														data-coppy=""
+														className="absolute inset-x-0 bottom-full mb-1 flex justify-center opacity-0 transition-opacity duration-200"
+													>
+														<span className="sr-only">Copy code</span>
+														<span className="bg-gray-900 text-white rounded-md text-[0.625rem] leading-4 tracking-wide font-semibold uppercase py-1 px-3 filter drop-shadow-md">
+															<svg
+																width="16"
+																height="6"
+																viewBox="0 0 16 6"
+																className="text-gray-900 absolute top-full left-1/2 -mt-px -ml-2"
+															>
+																<path
+																	fillRule="evenodd"
+																	clipRule="evenodd"
+																	d="M15 0H1V1.00366V1.00366V1.00371H1.01672C2.72058 1.0147 4.24225 2.74704 5.42685 4.72928C6.42941 6.40691 9.57154 6.4069 10.5741 4.72926C11.7587 2.74703 13.2803 1.0147 14.9841 1.00371H15V0Z"
+																	fill="currentColor"
+																></path>
+															</svg>
+															Copied
+														</span>
+													</div>
+													<svg
+														data-coppy=""
+														aria-hidden="true"
+														focusable="false"
+														className="w-4 h-auto ml-2 opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition-opacity duration-200"
+														role="img"
+														xmlns="http://www.w3.org/2000/svg"
+														viewBox="0 0 512 512"
+													>
+														<path
+															fill="currentColor"
+															d="M464 0H144c-26.51 0-48 21.49-48 48v48H48c-26.51 0-48 21.49-48 48v320c0 26.51 21.49 48 48 48h320c26.51 0 48-21.49 48-48v-48h48c26.51 0 48-21.49 48-48V48c0-26.51-21.49-48-48-48zM362 464H54a6 6 0 0 1-6-6V150a6 6 0 0 1 6-6h42v224c0 26.51 21.49 48 48 48h224v42a6 6 0 0 1-6 6zm96-96H150a6 6 0 0 1-6-6V54a6 6 0 0 1 6-6h308a6 6 0 0 1 6 6v308a6 6 0 0 1-6 6z"
+														></path>
+													</svg>
+												</div>
+											</dd>
+										</div>
+									</dl>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
 		</StaticSite>
 	);
 }
--- pages/stante-se-partnerem/index.js
+++ pages/stante-se-partnerem/index.js
@@ -1,10 +1,48 @@
+import {
+	ButtonLinkFilledBig,
+	ButtonLinkOutlineBig,
+	HeadingWrapper,
+	MainH2,
+} from "@components/atoms";
 import { StaticSite } from "@components/templates";
 
 export default function Partner() {
 	return (
 		<StaticSite>
 			{/* "Staňte se naším partnerem" + CTA */}
-
+			<section className="relative bg-white overflow-hidden">
+				<div className="max-w-7xl lg:w-1/2">
+					<div className="relative z-10 py-8 lg:bg-white sm:py-16 md:py-20 lg:max-w-2xl lg:w-full lg:py-28 xl:py-32 ml-auto">
+						<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-12 lg:py-16">
+							<div className="text-left">
+								<h1 className="text-4xl tracking-tight font-extrabold text-white lg:text-gray-900 sm:text-5xl md:text-6xl">
+									<span className="block">Staňte se naším</span>
+									<span className="block text-main-800">Partnerem</span>
+								</h1>
+								<p className="mt-3 text-base text-gray-200 lg:text-gray-600 sm:mt-5 sm:text-lg sm:max-w-xl md:mt-5 md:text-xl lg:mx-0">
+									Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat
+									commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+								</p>
+								<div className="mt-5 sm:mt-8 flex flex-wrap gap-3 justify-start">
+									<ButtonLinkFilledBig>Staňte se partnerem</ButtonLinkFilledBig>
+									<ButtonLinkOutlineBig>Naše síť</ButtonLinkOutlineBig>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="absolute inset-y-0 w-screen lg:right-0 lg:w-1/2">
+					<img
+						className="w-full h-full object-cover blur-sm lg:blur-0"
+						src="https://source.unsplash.com/random/?partner"
+						alt="Partner"
+					/>
+					<div
+						className="lg:hidden absolute bg-black bg-opacity-60 inset-0"
+						aria-hidden="true"
+					></div>
+				</div>
+			</section>
 			{/* Centered 2x2 grid "Co vám přinese partnerství" */}
 
 			<div className="py-12 bg-white">
@@ -37,9 +75,9 @@ export default function Partner() {
 											aria-hidden="true"
 										>
 											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												stroke-width="2"
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												strokeWidth="2"
 												d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
 											/>
 										</svg>
@@ -67,9 +105,9 @@ export default function Partner() {
 											aria-hidden="true"
 										>
 											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												stroke-width="2"
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												strokeWidth="2"
 												d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
 											/>
 										</svg>
@@ -97,9 +135,9 @@ export default function Partner() {
 											aria-hidden="true"
 										>
 											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												stroke-width="2"
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												strokeWidth="2"
 												d="M13 10V3L4 14h7v7l9-11h-7z"
 											/>
 										</svg>
@@ -127,9 +165,9 @@ export default function Partner() {
 											aria-hidden="true"
 										>
 											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												stroke-width="2"
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												strokeWidth="2"
 												d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
 											/>
 										</svg>
@@ -149,7 +187,42 @@ export default function Partner() {
 			</div>
 
 			{/* "Co znamená být naším partnerem" */}
-
+			<section className="relative">
+				<div className="flex flex-col mx-auto lg:flex-row">
+					<div className="relative px-4 mx-auto py-12 lg:py-16 lg:max-w-7xl lg:pl-6 lg:mr-auto">
+						<div className="text-base max-w-prose mx-auto lg:max-w-lg lg:mr-0">
+							<HeadingWrapper>
+								<MainH2> Our Process</MainH2>
+							</HeadingWrapper>
+							<div className="mt-5 prose prose-red text-gray-500">
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, excepturi tempore
+									repudiandae nihil ipsam officiis dolor deleniti ex maiores dicta commodi magni
+									sapiente vel id ipsum suscipit fugit, ea ducimus.
+								</p>
+								<h3>Some header</h3>
+								<p>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci nulla autem
+									porro quod aliquam itaque a dolore delectus praesentium maxime!
+								</p>
+								<ul>
+									<li>Lorem ipsum dolor sit.</li> <li>Lorem ipsum dolor sit.</li>
+									<li>Lorem ipsum dolor sit.</li>
+								</ul>
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, libero.</p>
+							</div>
+						</div>
+					</div>
+					<div className="lg:w-1/2 lg:h-auto flex items-center justify-center">
+						<img
+							className="h-56 w-full object-cover lg:h-full shadow-md"
+							src="https://source.unsplash.com/random/?garden"
+							alt=""
+							loading="lazy"
+						/>
+					</div>
+				</div>
+			</section>
 			{/* "Jednoduchá administrátorská konzole" */}
 
 			<div className="bg-white">
