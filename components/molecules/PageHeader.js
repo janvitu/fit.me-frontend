5bc6759cf27da6b904f3eda2d88c8bfb766d57ff
Lukáš Tragan <lukastragan@gmail.com>
2021-10-24 02:22:58 +0200

    page header props update

--- components/molecules/PageHeader.js
+++ components/molecules/PageHeader.js
@@ -4,12 +4,9 @@ import { HeadingText, SubheadingText, TopheadingText } from "@components/atoms";
 export function PageHeader({ topheadingText, headingText, subheadingText }) {
 	return (
 		<div className="lg:text-center">
-			<TopheadingText>Benefity</TopheadingText>
-			<HeadingText>Co vám přinese partnerství</HeadingText>
-			<SubheadingText>
-				Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate
-				veritatis in accusamus quisquam.
-			</SubheadingText>
+			<TopheadingText>{topheadingText}</TopheadingText>
+			<HeadingText>{headingText}</HeadingText>
+			<SubheadingText>{subheadingText}</SubheadingText>
 		</div>
 	);
 }
