c6148d6c22e3697e3967c54e25c55703f6f6e0e5
Jan Vítů <vitu.jan1@gmail.com>
2021-10-24 12:29:10 +0200

    next/link fix in components

--- components/atoms/NavLink.js
+++ components/atoms/NavLink.js
@@ -2,10 +2,10 @@ import Link from "next/link";
 export function NavLink({ href, children }) {
 	return (
 		<Link href={href} passHref>
-			<div className="group relative cursor-pointer hover:text-main-600 py-3">
+			<a className="group relative cursor-pointer hover:text-main-600 py-3">
 				{children}
 				<div className=" w-0 md:group-hover:w-full h-1 bg-main-600 transition-all" />
-			</div>
+			</a>
 		</Link>
 	);
 }
--- components/organisms/NavBar.js
+++ components/organisms/NavBar.js
@@ -15,7 +15,9 @@ export function NavBar() {
 				<div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
 					<div className="flex justify-start lg:w-0 lg:flex-1">
 						<Link href="/" passHref>
-							<Logo />
+							<a>
+								<Logo />
+							</a>
 						</Link>
 					</div>
 					<NavStatics routes={ROUTES} />
