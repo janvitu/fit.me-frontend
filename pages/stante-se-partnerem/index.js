ac7f89df8096cc2f9a60610b4f4a8a680435f62d
hous01 <hous01@vse.cz>
2021-11-03 17:10:08 +0100

    texty stante se partnerem

--- pages/stante-se-partnerem/index.js
+++ pages/stante-se-partnerem/index.js
@@ -8,27 +8,27 @@ const descriptionList = [
 	{
 		term: "Možnost přístupu do administrátorské konzole",
 		description:
-			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+			"Hned po registraci získáte přístup do administrátorské konzole! Intuitivní a krásné.",
 		svgDraw:
 			"M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9",
 	},
 	{
 		term: "Propojení s vaším firemním účtem",
 		description:
-			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+			"Máte deset různých účtů a deset různých hesel? Single sign on je u nás samozřejmostí.",
 		svgDraw:
 			"M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3",
 	},
 	{
 		term: "Vytváření události",
 		description:
-			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+			"Klikni, pak ještě jednou a událost je hotová. Jednoduché uživatelské prostředí je samozřejmostí.",
 		svgDraw: "M13 10V3L4 14h7v7l9-11h-7z",
 	},
 	{
 		term: "Možnost zasílat notifikací vašim zákazníkům",
 		description:
-			"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+			"Máte nenaplněnou sportovní lekci? Lákavou nabídku pro zákazníky? Nového trenéra, o kterém musí vědět svět? Zašlete notifikaci!",
 		svgDraw:
 			"M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z",
 	},
@@ -48,8 +48,8 @@ export default function Partner() {
 									<span className="block text-main-800">Partnerem</span>
 								</H1>
 								<MainDescription>
-									Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat
-									commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+									Se sportovišti komunikujeme jako s našimi rovnocennými partnery. Vaši zpětnou
+									vazbu bereme vážně a všechny vstupy sportovišť pečlivě zvažujeme.
 								</MainDescription>
 								<div className="mt-5 sm:mt-8 flex flex-wrap gap-3 justify-start">
 									<ButtonLink variant="big" isOutline={false}>
@@ -83,8 +83,7 @@ export default function Partner() {
 						<H2 variant="small">Benefity</H2>
 						<H2 variant="base">Co vám přinese partnerství</H2>
 						<MainDescription>
-							Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum
-							cupiditate veritatis in accusamus quisquam.
+							Staňte se součástí největší platformy spojující sportoviště a sportovce!
 						</MainDescription>
 					</div>
 					<DescriptionList list={descriptionList} />
@@ -97,24 +96,19 @@ export default function Partner() {
 					<div className="relative px-4 mx-auto py-12 lg:py-16 lg:max-w-7xl lg:pl-6 lg:mr-auto">
 						<div className="text-base max-w-prose mx-auto lg:max-w-lg lg:mr-0">
 							<HeadingWrapper>
-								<H2 variant="base">Our Process</H2>
+								<H2 variant="base">Jak pracujeme</H2>
 							</HeadingWrapper>
 							<SmallDescription>
 								<p>
-									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, excepturi tempore
-									repudiandae nihil ipsam officiis dolor deleniti ex maiores dicta commodi magni
-									sapiente vel id ipsum suscipit fugit, ea ducimus.
+									Se sportovišti komunikujeme jako s našimi rovnocennými partnery. Vaši zpětnou
+									vazbu bereme vážně a všechny vstupy sportovišť pečlivě zvažujeme.
 								</p>
-								<h3>Some header</h3>
-								<p>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci nulla autem
-									porro quod aliquam itaque a dolore delectus praesentium maxime!
-								</p>
+								<h3>Jak vypadá spolupráce</h3>
+								<p>Postup domluvení spolupráce se sportovištěm je intutivní a jednoduchý.</p>
 								<ul>
-									<li>Lorem ipsum dolor sit.</li> <li>Lorem ipsum dolor sit.</li>
-									<li>Lorem ipsum dolor sit.</li>
+									<li>První schůzka</li> <li>Podpis smlouvy</li>
+									<li>Registrace do platformy</li>
 								</ul>
-								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, libero.</p>
 							</SmallDescription>
 						</div>
 					</div>
@@ -144,8 +138,7 @@ export default function Partner() {
 							</p>
 						</H2>
 						<p className="my-4 text-lg leading-6 text-main-200">
-							Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing
-							sagittis vel nulla nec.
+							Hned po registraci získáte přístup do administrátorské konzole! Intuitivní a krásné.
 						</p>
 						<div className="md:max-w-xs">
 							<ButtonLink variant="base" outline={true} href="#">
