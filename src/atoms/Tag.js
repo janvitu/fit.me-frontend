00ada72674e9d909c52a021068b7eaaeca23e7d0
albertmirek <albert.mirek@seznam.cz>
2021-11-04 13:38:37 +0100

    Created tag atom

---
+++ src/atoms/Tag.js
@@ -0,0 +1,7 @@
+export function Tag({ name }) {
+	return (
+		<span className=" relative flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">
+			{name}
+		</span>
+	);
+}
