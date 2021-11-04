c0c332a12d2af40cb7e194843168d0e6b9c9c526
albertmirek <albert.mirek@seznam.cz>
2021-11-04 13:41:58 +0100

    Created email and chat button for cards

---
+++ src/atoms/ButtonCard.js
@@ -0,0 +1,40 @@
+import email from "@assets/img/email.svg";
+import chat from "@assets/img/message.svg";
+
+/**
+ * @param  {"email"/"chat"} {type}
+ */
+export function ButtonCard({ type, text }) {
+	const typeHandler = (type) => {
+		switch (type) {
+			case "email":
+				return {
+					text: "Email",
+					src: email.src,
+					alt: "Email Button",
+				};
+			case "chat":
+				return {
+					text: "Chat",
+					src: email.src,
+					alt: "Chat Button",
+				};
+			default:
+				break;
+		}
+	};
+
+	return (
+		<div className="w-0 flex-1 flex">
+			<a
+				// href="mailto:janecooper@example.com"
+				href="#"
+				className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
+			>
+				<img src={typeHandler(type).src} alt={typeHandler(type).alt} className=" w-6 h-6" />
+
+				<span className="ml-3">{typeHandler(type).text}</span>
+			</a>
+		</div>
+	);
+}
