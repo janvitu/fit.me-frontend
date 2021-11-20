42246ae0486478dbf888ca03627cf09af336983d
albertmirek <albert.mirek@seznam.cz>
2021-11-20 11:10:08 +0100

    Added rating indicator

--- src/molecules/TrainerCard.js
+++ src/molecules/TrainerCard.js
@@ -1,27 +1,33 @@
 import { ButtonCard, Tag } from "@src/atoms";
+import { RatingIndicator } from "@src/molecules";
 import hooliIcon from "@assets/img/hooli-brands.svg";
 import emailIcon from "@assets/img/email.svg";
 import messageIcon from "@assets/img/message.svg";
 import Link from "next/link";
-export function TrainerCard({ name, username, description, tags, img }) {
+import Image from "next/image";
+
+export function TrainerCard({ name, username, description, tags, img, rating }) {
 	return (
 		<div className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200">
 			<Link href={`/treneri/${username}`} passHref>
 				<a>
 					<div className="flex-1 flex flex-col p-8">
-						<img
+						<Image
+							width="128 px"
+							height="128 px"
 							className="w-32 h-32 flex-shrink-0 mx-auto rounded-full"
 							src={img.src}
 							alt={img.alt}
 						/>
 						<h3 className="mt-6 text-gray-900 text-sm font-medium">{name}</h3>
+						<RatingIndicator ratingValue={rating} className="self-center" />
 						<dl className="mt-1 flex-grow flex flex-col justify-between">
 							<dt className="sr-only">Description</dt>
 							<dd className="text-gray-500 text-sm">{description}</dd>
 							<dt className="sr-only">Specializations</dt>
-							<dd className="mt-3 space-x-1">
+							<dd className="mt-3 space-x-1 space-y-1">
 								{tags.map((tag, index) => (
-									<Tag name={tag.name} key={index} />
+									<Tag name={tag.name} color={tag.color} key={index} />
 								))}
 							</dd>
 						</dl>
@@ -41,18 +47,8 @@ export function TrainerCard({ name, username, description, tags, img }) {
 TrainerCard.defaultProps = {
 	name: "Robert Chott",
 	username: "robertchott",
-	description: "Fyzio Terapeut a Zen Master",
-	tags: [
-		{
-			name: "Fyzioterapie",
-		},
-		{
-			name: "Mistr chi",
-		},
-		{
-			name: "Zen master",
-		},
-	],
+	description: "Váš osobní trenér za cestou úlevy",
+
 	img: {
 		src: hooliIcon.src,
 		alt: "",
