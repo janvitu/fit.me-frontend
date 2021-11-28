aab88ce82e41cc12e37752d8d849640661d40132
Jan Vítů <vitu.jan1@gmail.com>
2021-11-28 16:06:26 +0100

    update of queries and mutations

--- src/organisms/LogInForm.js
+++ src/organisms/LogInForm.js
@@ -2,49 +2,44 @@ import { InputWrapper } from "@src/molecules";
 import { ButtonSubmit } from "@src/atoms";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import { gql, useMutation } from "@apollo/client";
+import { gql, useLazyQuery } from "@apollo/client";
 import Router from "next/router";
 import toast from "react-hot-toast";
+import { useEffect } from "react";
 
 const LOG_IN = gql`
-	mutation LogInSportsman($email: String!, $password: String!) {
-		sportsmanSignIn(email: $email, password: $password) {
+	query UserSignIn($email: String!, $password: String!, $accType: String) {
+		userSignIn(email: $email, password: $password, accType: $accType) {
+			user {
+				id
+				verified
+			}
 			token
 		}
 	}
 `;
 
 export function LogInForm() {
-	const [logInSportsman] = useMutation(LOG_IN);
+	const [userSignIn, { loading, error, data }] = useLazyQuery(LOG_IN);
+
+	useEffect(() => {
+		if (data && !loading) {
+			Router.push("/sportoviste");
+		}
+	}, [data, loading]);
 	const formik = useFormik({
 		initialValues: {
 			email: "",
 			password: "",
 		},
 		onSubmit: (values) => {
-			logInSportsman(
-				{
-					variables: {
-						email: values.email,
-						password: values.password,
-					},
+			userSignIn({
+				variables: {
+					email: values.email,
+					password: values.password,
+					accType: "sportsman",
 				},
-				toast.promise(logInSportsman(), {
-					loading: "Vyčkejte prosím, ověřují se údaje",
-					success: "Přihlášení bylo úspěšné! Vítejte",
-					error: "Přihlášení se nepovedlo, zkuste to znovu, nebo se obraťte na podporu",
-				}),
-			)
-				.then((res) => {
-					console.log(res);
-					Router.push("/sportoviste");
-				})
-				.catch((err) => {
-					console.error(err);
-				})
-				.finally(() => {
-					formik.resetForm();
-				});
+			});
 		},
 		validationSchema: Yup.object().shape({
 			email: Yup.string().email("Špatný formát emailu").required("Email nesmí být prázdný"),
